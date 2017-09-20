8364b6540924fe06169758dec336328143eb1fe8
Vitalik Priadka <vitalik.pryadka@gmail.com>
2017-09-20 20:07:18 +0300

    MainActivity added

---
+++ app/src/main/java/com/priadka/newsit_project/MainActivity.java
@@ -0,0 +1,12 @@
+package com.priadka.newsit_project;
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}

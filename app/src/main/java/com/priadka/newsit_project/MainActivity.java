f0c6b8420ca353829a9e1836555c909c03f275ad
Vitalik Priadka <vitalik.pryadka@gmail.com>
2017-09-20 22:04:41 +0300

    add Toolbar and searchButton

--- app/src/main/java/com/priadka/newsit_project/MainActivity.java
+++ app/src/main/java/com/priadka/newsit_project/MainActivity.java
@@ -1,12 +1,32 @@
 package com.priadka.newsit_project;
+
 import android.app.Activity;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 public class MainActivity extends Activity {
+
+    private  Toolbar toolbar;
+
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initToolbar();
+
     }
+
+    private void initToolbar(){
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle(R.string.app_name);
+        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener(){
+            @Override
+            public boolean onMenuItemClick(MenuItem menuItem) {
+                return false;
+            }
+        });
+        toolbar.inflateMenu(R.menu.menu);
+    }
+
 }

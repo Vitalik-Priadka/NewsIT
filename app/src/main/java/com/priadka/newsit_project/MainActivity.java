a9a6dafcd6fa2e3b93b53cc18614e5c41f20470b
Vitalik Priadka <vitalik.pryadka@gmail.com>
2017-09-21 19:56:04 +0300

    add AppBarLayout *Left*

--- app/src/main/java/com/priadka/newsit_project/MainActivity.java
+++ app/src/main/java/com/priadka/newsit_project/MainActivity.java
@@ -1,16 +1,17 @@
 package com.priadka.newsit_project;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
 
     private  Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.AppDefault);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initToolbar();

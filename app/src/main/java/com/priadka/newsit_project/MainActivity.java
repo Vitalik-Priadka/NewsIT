4d11510cbbd0599f3a8de8043353df938abdd7a9
Vitalik Priadka <vitalik.pryadka@gmail.com>
2017-09-21 21:32:28 +0300

    add AppBarLayout v1

--- app/src/main/java/com/priadka/newsit_project/MainActivity.java
+++ app/src/main/java/com/priadka/newsit_project/MainActivity.java
@@ -1,21 +1,23 @@
 package com.priadka.newsit_project;
 
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
-
+    private static  final int LAYOUT =R.layout.activity_main;
     private  Toolbar toolbar;
+    private DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         setTheme(R.style.AppDefault);
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(LAYOUT);
         initToolbar();
-
+        initNavigationView();
     }
 
     private void initToolbar(){
@@ -29,5 +31,8 @@ public class MainActivity extends AppCompatActivity {
         });
         toolbar.inflateMenu(R.menu.menu);
     }
+    private void initNavigationView() {
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
 
+    }
 }

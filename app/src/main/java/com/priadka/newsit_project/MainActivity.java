69d2d57627e74c51385cbc0f463c5f7ff86bcff5
Vitalik Priadka <vitalik.pryadka@gmail.com>
2017-09-22 19:20:58 +0300

    added NavigationView icon and exit function

--- app/src/main/java/com/priadka/newsit_project/MainActivity.java
+++ app/src/main/java/com/priadka/newsit_project/MainActivity.java
@@ -1,13 +1,16 @@
 package com.priadka.newsit_project;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
-    private static  final int LAYOUT =R.layout.activity_main;
+    private static  final int LAYOUT = R.layout.activity_main;
     private  Toolbar toolbar;
     private DrawerLayout drawerLayout;
 
@@ -22,7 +25,7 @@ public class MainActivity extends AppCompatActivity {
 
     private void initToolbar(){
         toolbar = (Toolbar) findViewById(R.id.toolbar);
-        toolbar.setTitle(R.string.app_name);
+        toolbar.setTitle(R.string.app_tittle);
         toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener(){
             @Override
             public boolean onMenuItemClick(MenuItem menuItem) {
@@ -31,8 +34,28 @@ public class MainActivity extends AppCompatActivity {
         });
         toolbar.inflateMenu(R.menu.menu);
     }
+
     private void initNavigationView() {
         drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
-
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout , toolbar,
+                                       R.string.view_navigation_open, R.string.view_navigation_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+        NavigationView navigationView = (NavigationView) findViewById(R.id.navigation);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                drawerLayout.closeDrawers();
+                switch (menuItem.getItemId()){
+                    case R.id.actionLogInItem: break;
+                    case R.id.actionNewsItem: break;
+                    case R.id.actionSettingItem: break;
+                    case R.id.actionExitItem: {
+                        finishAndRemoveTask (); break;}
+                }
+                return true;
+            }
+        });
     }
+
 }
